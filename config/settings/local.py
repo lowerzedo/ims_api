c184e939dc5d3d1e36d8c9e1c4f77e8276ffba37
Nazar Khasanov <nazarkhasanov@Nazars-MacBook-Pro.local>
2025-10-17 18:23:33 +0100

    chore: update CSRF_TRUSTED_ORIGINS to include production API endpoint

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,7 +3,7 @@ from .base import *  # noqa
 
 DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", "http://localhost:8000", "http://0.0.0.0:8000"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", "http://localhost:8000", "http://0.0.0.0:8000", "http://ims-api-k3j2.onrender.com"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 INSTALLED_APPS += ["django_extensions"]  # type: ignore # noqa: F405
